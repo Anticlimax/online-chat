ed7f824fed72af7bb516b57ecc347d807caae688
anticlimax <121777824@qq.com>
2017-11-29 10:33:04 +0800

    修改用户名和密码

--- src/main/sidebar/Sidebar.js
+++ src/main/sidebar/Sidebar.js
@@ -1,10 +1,35 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import SidebarItem from './sidebarItem/SidebarItem'
 
 class Sidebar extends Component {
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeGroup: [
+        {
+          name: 'test',
+          unreadMessage: 3,
+          latestMessage: 'hello'
+        }, {
+          name: 'test',
+          unreadMessage: 3,
+          latestMessage: 'hello'
+        }, {
+          name: 'test',
+          unreadMessage: 3,
+          latestMessage: 'hello'
+        }
+      ]
+    }
+  }
+
+  render() {
     return (
       <div>
         Sidebar
+        {this.state.activeGroup.map(({name, unreadMessage, latestMessage})=>{
+          return <SidebarItem name={name} unreadMessage={unreadMessage} latestMessage={latestMessage}></SidebarItem>  
+        })}
       </div>
     )
   }
--- src/main/sidebar/sidebarItem/SidebarItem.js
+++ src/main/sidebar/sidebarItem/SidebarItem.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
+import './SidebarItem.less'
+// class SidebarItem extends Component {
+//   render () {
+//     return (
+//       <div className="sidebarItem">
+//         SidebarItem
+//         name: {this.props.name}|
+//         unreadMessage: {this.props.unreadMessage}|
+//         latestMessage: {this.props.latestMessage}
+//       </div>
+//     )
+//   }
+// }
 
-class SidebarItem extends Component {
-  render () {
-    return (
-      <div>
-        SidebarItem
-      </div>
-    )
-  }
+export const SidebarItem = ({name, unreadMessage, latestMessage}) => {
+  return (
+    <div className="sidebarItem">
+    SidebarItem
+    name: {name}|
+    unreadMessage: {unreadMessage}|
+    latestMessage: {latestMessage}
+  </div>
+  )
 }
 
 export default SidebarItem
